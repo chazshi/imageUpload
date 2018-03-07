1e938b16a794f5e6dca7e32da08ebbe81f659bd1
chazshi <sczhi55@gmail.com>
2018-03-07 21:22:25 +0800

    some little change

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,41 +5,41 @@ var formidable = require("formidable");
 var iconv = require('iconv-lite');	//转换编码，解决utf-8和gbk编码转换问题（菱形问号，锟斤拷）
 
 
-// //4.处理监听请求
-// function start(response, request){
-// 	console.log("'start'请求处理...");
+//4.处理监听请求
+function start(response, request){
+	console.log("'start'请求处理...");
 	 
-// 	// //单文本框
-// 	// var body =  '<html>'+
-// 	// 			'<head>'+
-// 	// 			'<meta http-equiv="Content-Type" content="text/html; '+'charset=utf-8" />'+
-// 	// 			'</head>'+
-// 	// 			'<body>'+
-// 	// 			'<form action="/upload" method="post">'+
-// 	// 			'<textarea name="text" rows="20" cols="60"></textarea>'+
-// 	// 			'<input type="submit" value="提交" />'+
-// 	// 			'</form>'+
-// 	// 			'</body>'+
-// 	// 			'</html>';
+	// //单文本框
+	// var body =  '<html>'+
+	// 			'<head>'+
+	// 			'<meta http-equiv="Content-Type" content="text/html; '+'charset=utf-8" />'+
+	// 			'</head>'+
+	// 			'<body>'+
+	// 			'<form action="/upload" method="post">'+
+	// 			'<textarea name="text" rows="20" cols="60"></textarea>'+
+	// 			'<input type="submit" value="提交" />'+
+	// 			'</form>'+
+	// 			'</body>'+
+	// 			'</html>';
 	
-// 	//图片上传框
-// 	var body = 	'<html>'+
-// 				'<head>'+'<meta http-equiv="Content-Type" '+
-// 				'content="text/html; charset=UTF-8" />'+
-// 				'</head>'+
-// 				'<body>'+
-// 				'<form action="/upload" enctype="multipart/form-data" '+
-// 				'method="post">'+
-// 				'<input type="file" name="upload">'+
-// 				'<input type="submit" value="上传文件(jpg)" />'+
-// 				'</form>'+
-// 				'</body>'+
-// 				'</html>';
+	//图片上传框
+	var body = 	'<html>'+
+				'<head>'+'<meta http-equiv="Content-Type" '+
+				'content="text/html; charset=UTF-8" />'+
+				'</head>'+
+				'<body>'+
+				'<form action="/upload" enctype="multipart/form-data" '+
+				'method="post">'+
+				'<input type="file" name="upload">'+
+				'<input type="submit" value="上传文件(jpg)" />'+
+				'</form>'+
+				'</body>'+
+				'</html>';
 
-// 	response.writeHead(200, {"Content-Type":"text/html;charset=UTF-8"});
-// 	response.write(body);
-// 	response.end();
-// }
+	response.writeHead(200, {"Content-Type":"text/html;charset=UTF-8"});
+	response.write(body);
+	response.end();
+}
 
 function upload(response, request){
 	console.log("'upload'请求处理...");
@@ -89,18 +89,18 @@ function show(response, request){
 // }
 
 //测试非阻塞操作//###这里timeout: 10000好像没用
-function start(response){
-	console.log("4.start handle");
-	//4.处理监听请求
-	exec("dir", { encoding: 'binary', timeout: 10000, maxBuffer: 20000*1024}, function(error, stdout, stderr){
-		var encoding = 'gbk';	////或者'cp936'，cp936是gbk的内码表
-		var binaryEncoding = 'binary';
-		var str = iconv.decode(new Buffer(stdout, binaryEncoding), encoding);
-		response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
-		response.write(str);
-		response.end();
-	});
-}
+// function start(response){
+// 	console.log("4.start handle");
+// 	//4.处理监听请求
+// 	exec("dir", { encoding: 'binary', timeout: 10000, maxBuffer: 20000*1024}, function(error, stdout, stderr){
+// 		var encoding = 'gbk';	////或者'cp936'，cp936是gbk的内码表
+// 		var binaryEncoding = 'binary';
+// 		var str = iconv.decode(new Buffer(stdout, binaryEncoding), encoding);
+// 		response.writeHead(200, {"Content-Type":"text/plain;charset=utf-8"});
+// 		response.write(str);
+// 		response.end();
+// 	});
+// }
 
 exports.start = start;
 exports.upload = upload;
